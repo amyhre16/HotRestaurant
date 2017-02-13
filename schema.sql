02b192a00c9ee03349747fd5b6e2dc61a1f0d6ba
Anthony Myhre <anthony.myhre@utexas.edu>
2017-02-12 22:58:38 -0600

    changed Amy to Anthony on all html pages removed gif background on all html pages dropped tables in database. Recreated them with reservation_id and waitingList_id columns and made them the primary keys

--- schema.sql
+++ schema.sql
@@ -4,31 +4,21 @@ USE hotRestaurant;
 */
 
 CREATE TABLE IF NOT EXISTS tables (
-	-- reservation_id INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL
-	name VARCHAR(100) NOT NULL
+	reservation_id INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL
+	, name VARCHAR(100) NOT NULL
 	, phone_number VARCHAR(100)
 	, email VARCHAR(100)
 	, unique_id VARCHAR(100) NOT NULL
-	, PRIMARY KEY(unique_id)
+	, PRIMARY KEY(reservation_id)
 );
 
 
 
 CREATE TABLE IF NOT EXISTS waitingList (
-	-- waitingList_id INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL
-	name VARCHAR(100) NOT NULL
+	waitingList_id INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL
+	, name VARCHAR(100) NOT NULL
 	, phone_number VARCHAR(100)
 	, email VARCHAR(100)
 	, unique_id VARCHAR(100) NOT NULL
-	, PRIMARY KEY(unique_id)
+	, PRIMARY KEY(waitingList_id)
 );
-
-INSERT INTO tables
-(name, phone_number, email, unique_id)
-VALUES
-("Anthony", "281-330-8004", "test@test.com", "a");
-
-INSERT INTO waitingList
-(name, phone_number, email, unique_id)
-VALUES
-("Jenny", "867-5309", "jenny@aol.com", "j");
