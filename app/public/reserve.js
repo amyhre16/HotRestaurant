8b181d13325193e90da35b6054ddaa0fd3ca08fc
Anthony Myhre <anthony.myhre@utexas.edu>
2017-02-12 23:25:30 -0600

    added function in reserve.js that validates form inputs. if user filled out all fields, then continue adding the user data. otherwise, display the error modal

--- app/public/reserve.js
+++ app/public/reserve.js
@@ -13,37 +13,49 @@ $(document).ready(function() {
             unique: $("#unique").val().trim()
         };
 
-        for (var property in table) {
-            console.log(property);
+        if (formValidation(table)) {
+            // grab the domain from the url
+            var currentURL = window.location.origin;
+
+            /*
+                ajax post request with currentURL concatenated with the post route
+                send the table object
+                when the request is finished, run the callback function
+            */
+            $.post(currentURL + "/../../makeReservation", table, function(data) {
+                // if data has the reservation property, then let the user know that they've gotten a table
+                if(data.reservation != undefined){
+                    // alert("you got a table");
+                    $('#reservationModal').modal('toggle');
+                }
+                // if data has waiting property, let the user know that they've been added to the waiting list
+                else if (data.waiting != undefined) {
+                    // alert("you are on waiting list");
+                    $('#waitingListModal').modal('toggle');
+                }
+            });
+
+            // empty out the 
+            $("#name").val("");
+            $("#number").val("");
+            $("#email").val("");
+            $("#unique").val("");
         }
-        // grab the domain from the url
-        var currentURL = window.location.origin;
-
-        /*
-            ajax post request with currentURL concatenated with the post route
-            send the table object
-            when the request is finished, run the callback function
-        */
-        $.post(currentURL + "/../../makeReservation", table, function(data) {
-            // if data has the reservation property, then let the user know that they've gotten a table
-            if(data.reservation != undefined){
-                // alert("you got a table");
-                $('#reservationModal').modal('toggle');
-            }
-            // if data has waiting property, let the user know that they've been added to the waiting list
-            else if (data.waiting != undefined) {
-                // alert("you are on waiting list");
-                $('#waitingListModal').modal('toggle');
-            }
-        });
-
-        // empty out the 
-        $("#name").val("");
-        $("#number").val("");
-        $("#email").val("");
-        $("#unique").val("");
+        else {
+            $('#errorModal').modal('toggle');
+        }
         
         // return false to keep page from refreshing
         return false;
     });
+
+    function formValidation(table) {
+        for (var property in table) {
+            if (table[property] === "") {
+                return false;
+            }
+            console.log(table[property]);
+        }
+        return true;
+    }
 });
